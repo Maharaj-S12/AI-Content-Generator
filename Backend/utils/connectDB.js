f9db782af94a56b46570ed7c385e15cbdf94a83e
Maharaj S <85568715+Maharaj-S12@users.noreply.github.com>
2024-06-27 18:41:13 +0530

    Update connectDB.js

--- Backend/utils/connectDB.js
+++ Backend/utils/connectDB.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-//pass:KkOcrAgGqFOWxfbN/maharajshankar012
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://maharajshankar012:KkOcrAgGqFOWxfbN@cluster0.vjapmhl.mongodb.net/mern-ai?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    const conn = await mongoose.connect();
     console.log(`Mangodb connected ${conn.connection.host}`);
   } catch (error) {
     console.error(`error connecting to MangoDB ${error.message}`);
